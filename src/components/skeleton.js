e4ca9b57f40d60df0b826494ef2da5df4becb249
dihanto <dihanto2306@gmail.com>
2023-10-25 17:42:03 +0700

    🚀 add animation to skeleton

--- src/components/skeleton.js
+++ src/components/skeleton.js
@@ -1,20 +1,21 @@
 export default function SkeletonLoader() {
-    return (
-      <div className='w-[1000px]'>
-        {[1, 2, 3].map((placeholder, index) => (
-          <div key={index} className='bg-slate-50 pb-3 text-left text-sm'>
-            <div className='w-[500px] mx-auto'>
-              {/* User placeholder */}
-            </div>
-            <div className='flex items-center justify-center w-[500px] h-[500px] bg-gray-300 mx-auto'>
-              {/* Image placeholder */}
-            </div>
-            <div className='w-[500px] mx-auto'>
-              {/* Like and comment placeholders */}
-            </div>
-            <div className='border-b border-slate-300 w-[500px] mx-auto'></div>
+  return (
+    <div className="w-[1000px]">
+      {[1, 2, 3].map((index) => (
+        <div key={index} className="bg-slate-50 pb-3 text-left text-sm animate-pulse">
+          <div className="w-[500px] mx-auto">
+            <div className="h-4 bg-gray-300 rounded-lg"></div>
           </div>
-        ))}
-      </div>
-    );
-  }
+          <div className="flex items-center justify-center w-[500px] h-[500px] bg-gray-300 mx-auto">
+          </div>
+          <div className="w-[500px] mx-auto">
+            <div className="h-4 bg-gray-300 rounded-lg"></div>
+          </div>
+          <div className="border-b border-slate-300 w-[500px] mx-auto">
+            <div className="h-2 bg-gray-300"></div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
