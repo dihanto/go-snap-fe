0b167b7fe45f795c3d71ca0a9348f0e530b50137
dihanto <dihanto2306@gmail.com>
2023-10-28 23:43:36 +0700

    🐛 (profile) fix navbar not fullfill the screen

--- src/components/profile.js
+++ src/components/profile.js
@@ -39,48 +39,55 @@ export default function Profile({ token, userLogin, followings }) {
   }
 
   useEffect(() => {
+    if(!followings){
+      setFollowingCount(0)
+    } else{
+    setFollowingCount(followings.length)
+    }
     getPhoto();
     getFollower();
-    setFollowingCount(followings.length)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userLogin]);
 
   return (
-    <div className="bg-white p-4 flex">
+    <div>
       <Navbar />
-      <div className="w-1/5"></div>
-      <div className="w-4/5 px-20"> 
-        <div className="flex justify-center mt-5 text-xs">
-            <div className="w-44 h-44 bg-yellow-100">
-                {/* <img
-                src="your-profile-picture-url.jpg"
-                alt="Your Profile"
-                className="w-16 h-16 rounded-full"
-                /> */}
-            </div>
-            <div>
-                <div>
-                <h1 className=" font-semibold">{userLogin}</h1>
-                <p className="text-gray-600">Bio</p>
-                </div>
-                <div className="flex mt-3">
-                    <p className="mr-7"><span className="font-semibold">{userPhoto.length}</span> posts</p>
-                    <p className="mr-7"><span className="font-semibold">{followerCount}</span> followers</p>
-                    <p className="mr-7"><span className="font-semibold">{followingCount}</span> following</p>
-                </div>
-            </div>
-        </div>
+      <div className="bg-white p-4 flex">
+        
+        <div className="w-1/5"></div>
+        <div className="w-4/5 px-20"> 
+          <div className="flex justify-center mt-5 text-xs">
+              <div className="w-44 h-44 bg-yellow-100">
+                  {/* <img
+                  src="your-profile-picture-url.jpg"
+                  alt="Your Profile"
+                  className="w-16 h-16 rounded-full"
+                  /> */}
+              </div>
+              <div>
+                  <div>
+                  <h1 className=" font-semibold">{userLogin}</h1>
+                  <p className="text-gray-600">Bio</p>
+                  </div>
+                  <div className="flex mt-3">
+                      <p className="mr-7"><span className="font-semibold">{userPhoto.length}</span> posts</p>
+                      <p className="mr-7"><span className="font-semibold">{followerCount}</span> followers</p>
+                      <p className="mr-7"><span className="font-semibold">{followingCount}</span> following</p>
+                  </div>
+              </div>
+          </div>
 
-        <div className="grid grid-cols-3 gap-1 mt-4">
-            {userPhoto.map((photo) => (
-            <div key={photo.id} className="relative aspect-square">
-                <img
-                src={photo.photoBase64}
-                alt={photo.title}
-                className="object-cover w-full h-full rounded-sm"
-                />
-            </div>
-            ))}
+          <div className="grid grid-cols-3 gap-1 mt-4">
+              {userPhoto.map((photo) => (
+              <div key={photo.id} className="relative aspect-square">
+                  <img
+                  src={photo.photoBase64}
+                  alt={photo.title}
+                  className="object-cover w-full h-full rounded-sm"
+                  />
+              </div>
+              ))}
+          </div>
         </div>
       </div>
     </div>
